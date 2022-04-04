a7a5ded795da428c3264d0dc26a0411d47892671
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2022-04-04 00:36:30 -0400

    Create findingfiles.py

---
+++ findingfiles.py
@@ -0,0 +1,11 @@
+import os
+  
+os.system("touch /home/student/mycode/SLAPPY")
+
+for x in range(10):
+    if x < 3:
+        os.system(f"touch /home/student/file{x}.horgledorgle")
+    elif x < 7:
+        os.system(f"touch /home/file{x}.horgledorgle")
+    else:
+        os.system(f"touch /home/student/static/file{x}.horgledorgle")
