29f22992aee927c89b60f6bbdbfc119caac861b5
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2022-05-17 10:22:19 -0400

    Update catfiles.sh

--- catfiles.sh
+++ catfiles.sh
@@ -1,5 +1,5 @@
-mkdir /home/student/haiku
+mkdir -p /home/student/haiku
 
-echo "Haikus are easy." > ~/haiku/file1.txt
-echo "But sometimes they don't make sense." > ~/haiku/file2.txt
-echo "Refrigerator." > ~/haiku/file3.txt
+echo "Haikus are easy." > ~/haiku/apple.txt
+echo "But sometimes they don't make sense." > ~/haiku/kumquat.txt
+echo "Refrigerator." > ~/haiku/dragonfruit.txt
