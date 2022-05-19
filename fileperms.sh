0ea015eaa07b93fee4ae7bac18874b437ad4e0d6
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2022-05-19 10:29:34 -0400

    Update fileperms.sh

--- fileperms.sh
+++ fileperms.sh
@@ -1,7 +1,8 @@
 sudo useradd chad
-sudo mkdir -p /home/student/challenge/dir2
-sudo echo "Happy Thursday!" > /home/student/challenge/file1
-sudo touch /home/student/challenge/dir2/file2
+mkdir -p /home/student/challenge/dir2
+touch /home/student/challenge/file1
+echo "Happy Thursday!" > /home/student/challenge/file1
+touch /home/student/challenge/dir2/file2
 sudo chown chad:chad /home/student/challenge/dir2 /home/student/challenge/file1 /home/student/challenge/dir2/file2 /home/student/challenge/
 
 sudo chmod o=rx /home/student/challenge/dir2
